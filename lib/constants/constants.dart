3d5a44030f367db311e61e7cd390874224028947
rus0n <antonio.cruz@prosfy.com>
2023-08-26 20:46:17 +0200

    feature: pages auth

---
+++ lib/constants/constants.dart
@@ -0,0 +1,6 @@
+const String APPWRITE_PROJECT_ID = "64e0be3b7cb9372242ef";
+const String APPWRITE_URL = "https://cloud.appwrite.io/v1";
+
+//credenciales de las colecciones
+const String APPWRITE_DATABASE_ID = "643d04665fd8051f19be";
+const String COLLECTION_MESSAGES = "643d047649b1cf70e36e";
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,45 @@
+import 'package:responsive_framework/breakpoint.dart';
+import 'package:responsive_framework/responsive_breakpoints.dart';
+
+import './pages/login_page.dart';
+import './pages/tabs_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'appwrite/auth_api.dart';
 
 void main() {
-  runApp(const MainApp());
+  // runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+      create: ((context) => AuthAPI()), child: const MyApp()));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    final value = context.watch<AuthAPI>().status;
+    print('TOP CHANGE Value changed to: $value!');
+
+    return MaterialApp(
+        title: 'BugKiller',
+        debugShowCheckedModeBanner: false,
+        builder: (context, child) => ResponsiveBreakpoints.builder(
+              child: child!,
+              breakpoints: const [
+                Breakpoint(start: 0, end: 450, name: MOBILE),
+                Breakpoint(start: 451, end: 800, name: TABLET),
+                Breakpoint(start: 801, end: 1920, name: DESKTOP),
+              ],
+            ),
+        home: value == AuthStatus.uninitialized
+            ? const Scaffold(
+                body: Center(child: CircularProgressIndicator()),
+              )
+            : value == AuthStatus.authenticated
+                ? const TabsPage()
+                : const LoginPage(),
+        theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.red));
   }
 }
